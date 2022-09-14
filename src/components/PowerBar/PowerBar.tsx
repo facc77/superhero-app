220a99ff80cc32867f5e0b14b357cf05726604ca
facc77 <ferrerfacundo96@gmail.com>
2022-09-14 16:10:10 -0300

    hero stats bar added

---
+++ src/components/PowerBar/PowerBar.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+import type { ChartData, ChartOptions } from 'chart.js';
+import { CategoryScale } from 'chart.js';
+import Chart from 'chart.js/auto';
+Chart.register(CategoryScale);
+
+interface PowerBarProps {
+  intelligence: string;
+  durability: string;
+  combat: string;
+  power: string;
+  speed: string;
+  strength: string;
+}
+
+interface LineProps {
+  options: ChartOptions<'bar'>;
+  data: ChartData<'bar'>;
+}
+
+const PowerBar: React.FC<PowerBarProps> = (powerStats) => {
+  const { intelligence, durability, combat, power, speed, strength } =
+    powerStats;
+
+  return (
+    <div className={`statsContainer `}>
+      {/* ${heroTeam ? "cardTeam" : null} */}
+      <Bar
+        data={{
+          labels: [
+            'Combat',
+            'Durability',
+            'Intelligence',
+            'Power',
+            'Speed',
+            'Strength',
+          ],
+          datasets: [
+            {
+              label: 'power stats',
+              data: [
+                parseInt(combat),
+                parseInt(durability),
+                parseInt(intelligence),
+                parseInt(power),
+                parseInt(speed),
+                parseInt(strength),
+              ],
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+              ],
+              borderWidth: 1,
+            },
+          ],
+        }}
+        height={200}
+        width={300}
+        options={{
+          indexAxis: 'y',
+          maintainAspectRatio: false,
+          /* scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                },
+              },
+            ],
+          },
+          legend: {
+            labels: {
+              font: { size: 50 },
+            },
+          },  */
+        }}
+      ></Bar>
+    </div>
+  );
+};
+
+export default PowerBar;
---
+++ src/components/PowerBar/index.tsx
@@ -0,0 +1,1 @@
+export { default as PowerBar } from './PowerBar';
