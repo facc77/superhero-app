63e2874b7b342aa786f84595339cfda9d505cd3b
facc77 <ferrerfacundo96@gmail.com>
2022-09-14 16:06:06 -0300

    background video settings added

---
+++ src/components/BackgroundVideo/BackgroundVideo.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+import Background from '../../assets/background.mp4';
+
+const BackgroundVideo: React.FC = () => {
+  return (
+    <video autoPlay muted loop playsInline id='myVideo'>
+      <source src={Background} type='video/mp4' id='myVideo1' />
+    </video>
+  );
+};
+
+export default BackgroundVideo;
