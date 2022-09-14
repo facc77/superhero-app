d1bee931d10f42d2f77b94b54fdd9b384407342d
facc77 <ferrerfacundo96@gmail.com>
2022-09-14 16:01:33 -0300

    added pages routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+import Home from './pages/Home';
+import TeamSection from './pages/TeamSection';
+import SearchResults from './pages/SearchResults';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/teamSection' element={<TeamSection />} />
+        <Route path='/searchResults' element={<SearchResults />} />
+      </Routes>
+    </Router>
   );
 }
 
