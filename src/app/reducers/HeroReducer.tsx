09cd5b4089741b0508bd3a24a49ea376c83487da
facc77 <ferrerfacundo96@gmail.com>
2022-09-26 13:28:34 -0300

    added setsearch reducer

--- src/app/reducers/HeroReducer.tsx
+++ src/app/reducers/HeroReducer.tsx
@@ -1,55 +1,20 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-
-interface Hero {
-  appearance: {
-    'eye-color': string;
-    'hair-color': string;
-    gender: string;
-    height: [string, string];
-    weight: [string, string];
-    race: string;
-  };
-  biography: {
-    aliases: string[];
-    aligment: string;
-    'alter-egos': string;
-    'first-appearance': string;
-    'full-name': string;
-    'place-of-birth': string;
-    publisher: string;
-  };
-  connections: {
-    'group-affiliation': string;
-    relatives: string;
-  };
-  id: string;
-  name: string;
-  image?: { url: string } | null;
-  powerstats: {
-    combat: string;
-    durability: string;
-    intelligence: string;
-    power: string;
-    speed: string;
-    strength: string;
-  };
-  work: {
-    occupation: string;
-    base: string;
-  };
-}
-
-interface CardProps {
-  hero: Hero;
-  team: Boolean;
-}
+import { Hero } from '../../types/Hero';
 
 type InitialState = {
   heroes: Hero[];
+  search: string | null;
+  heroSelected: Hero | null;
 };
 
+const team = JSON.parse(localStorage.getItem('team') || '[]');
+const selectedHero = JSON.parse(localStorage.getItem('selectedHero') || '[]');
+const savedSearch = window.localStorage.getItem('search');
+
 const initialState: InitialState = {
-  heroes: [],
+  heroes: team,
+  search: savedSearch,
+  heroSelected: selectedHero,
 };
 
 const HeroSlice = createSlice({
@@ -58,22 +23,21 @@ const HeroSlice = createSlice({
   reducers: {
     setAddHero(state, action: PayloadAction<Hero>) {
       state.heroes = [...state.heroes, action.payload];
-      console.log('agregar producto');
-      /* toast.success(`${action.payload.name} agregado al carrito!`, {
-                position: "bottom-left",
-              });   */
     },
     setRemoveHero(state, action: PayloadAction<string>) {
       state.heroes = state.heroes.filter(
         (heroe) => heroe.id !== action.payload,
       );
-      console.log('heroe eliminado');
-      /* toast.success(`${action.payload.name} agregado al carrito!`, {
-                position: "bottom-left",
-              });   */
+    },
+    setSearchValue(state, action: PayloadAction<string>) {
+      state.search = action.payload;
+    },
+    setSelectedHero(state, action: PayloadAction<Hero>) {
+      state.heroSelected = action.payload;
     },
   },
 });
 
-export const { setAddHero, setRemoveHero } = HeroSlice.actions;
+export const { setAddHero, setRemoveHero, setSearchValue, setSelectedHero } =
+  HeroSlice.actions;
 export default HeroSlice.reducer;
